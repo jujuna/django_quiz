e6cc9d8671469c1aacf67dc3caaff92c4f9b0982
Misho <nnamper@gmail.com>
2021-02-11 23:57:08 +0400

    removed .vscode

--- Website/home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
-
-class Useradmin(admin.ModelAdmin):
-    list_display=("username","first_name","email","mob")
-    fields=("username","first_name","email","mob","password",)
-
-admin.site.register(User, Useradmin)
